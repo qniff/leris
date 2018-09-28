173a3f678a78d3dfcd851a582245e516bd8331b7
Artjom Leonov <artjom.leonov@silverstreet.com>
2018-09-28 09:22:11 +0800

    minified footer pic

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ const app = express()
 var ejs = require('ejs')
 var bodyParser = require('body-parser')
 var path = require('path')
+var server = require('http').createServer(app)
+var port = process.env.PORT || 3000
 
 app.engine('html', ejs.renderFile)
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'html')
 
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public/'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 require('./routes')(app)
 
-app.listen(8000, () => console.log('Father app is listening on port 8000!'))
+server.listen(port, () => {
+  console.log('Server listening at port %d', port)
+})
